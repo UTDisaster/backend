b9d0f2182f4e60570fc19d644754b6ce7943e486
Swapnil Singh <theswapnilsingh11@gmail.com>
2026-02-24 21:39:26 -0600

    wip: incorrect mock locations

--- app/main.py
+++ app/main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
+
+MOCK_LOCATIONS = {
+    "disaster-123": [
+        {
+            "id": "p1",
+            "lat": 34.0522,
+            "lng": -118.2437,
+            "damage": "High",
+            "img": "thumb_01.jpg",
+        },
+        {
+            "id": "p2",
+            "lat": 34.0622,
+            "lng": -118.2537,
+            "damage": "Medium",
+            "img": "thumb_02.jpg",
+        },
+    ]
+}
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+async def health():
+    return {"status": "OK", "status_code": status.HTTP_200_OK}
+
+
+@app.get("/location/{disaster_id}")
+async def get_location(disaster_id: str):
+    data = MOCK_LOCATIONS.get(disaster_id)
+    if data:
+        return data
+
+    raise HTTPException(status_code=404, detail="Disaster not found")
