a64c0df15c85d44934934e71f10e994bd0e639a9
Swapnil Singh <theswapnilsingh11@gmail.com>
2026-02-26 13:13:00 -0600

    modify message for root

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "UTD Disaster Assessment Project"}
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health():
